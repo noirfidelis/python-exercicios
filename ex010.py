f441051c19f0203ab156350dfd19fe3fbf47d7ee
Noir Fidelis <noirfidelis@gmail.com>
2022-08-18 07:29:32 -0300

    atualização

--- ex010.py
+++ ex010.py
@@ -1,10 +1,10 @@
-#Conversor de Moedas
+#Conversor de moedas
 d = float(input('Tem quanto de grana ai? R$'))
-dollar = d / 5.28
-euro = d / 5.37
-libra = d / 6.43
-iene = d / 0.040
-bitcoin = d / 121.466 #misericórdia
+dollar = d / 5.17
+euro = d / 5.25
+libra = d / 6.24
+iene = d / 0.038
+bitcoin = d / 121.489 #misericórdia
 print(f'Com R$ {d:.2f} da pra você comprar:')
 print(f'US$ {dollar:.2f}')
 print(f'€ {euro:.2f}')
